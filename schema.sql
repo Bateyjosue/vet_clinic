2b74d0f6be9613436153f71c594c6755b099bbd3
Bateyjosue <josuebatey19@gmail.com>
2022-10-25 02:24:28 +0200

    Add the schema, and animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+-- Create schema
+create database vet_clinic;
+
+-- Connect the database
+\c vet_clinic;
+
+-- Create animals Table
+create table animals (id int GENERATED ALWAYS AS IDENTITY, name varchar(255), date_of_birth date, escape_attempts int, neutered boolean, weight_kg float, PRIMARY KEY (id));
