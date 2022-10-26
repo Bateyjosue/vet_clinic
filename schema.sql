a72373086955fcb861c83a5b2df7e64ab876a44a
Bateyjosue <josuebatey19@gmail.com>
2022-10-27 00:51:07 +0200

    Add Table Species

--- schema.sql
+++ schema.sql
@@ -10,10 +10,15 @@ create database vet_clinic;
 create table animals (id int GENERATED ALWAYS AS IDENTITY, name varchar(255), date_of_birth date, escape_attempts int, neutered boolean, weight_kg float, PRIMARY KEY (id));
 
 create table owners (
-  id int autoincrement PRIMARY KEY,
+  id serial PRIMARY KEY,
   full_name varchar(255),
   age int
-)
+);
+
+create table species (
+  id serial primary key,
+  name varchar(255),
+);
 
 -- Add column Spcies
 alter table animals
